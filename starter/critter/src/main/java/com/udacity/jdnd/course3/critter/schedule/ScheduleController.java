908083972a14747d7137f0c5830c6d0ca6d62530
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-04-12 05:24:56 +0200

    Injected Employee and Pet Service, created Dto Conversion Methods

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,7 +1,15 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.data.Employee;
+import com.udacity.jdnd.course3.critter.data.Pet;
+import com.udacity.jdnd.course3.critter.data.Schedule;
+import com.udacity.jdnd.course3.critter.service.EmployeeService;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +18,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
+
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @Autowired
+    PetService petService;
 
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
@@ -34,5 +49,50 @@ public class ScheduleController {
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
         throw new UnsupportedOperationException();
+    }
+
+
+    private Schedule convertFromScheduleDto(ScheduleDTO scheduleDTO){
+        Schedule schedule = new Schedule();
+        BeanUtils.copyProperties(scheduleDTO, schedule);
+        if (scheduleDTO.getEmployeeIds() != null){
+            List<Employee> employees = new ArrayList<>();
+            for (Long employeeid: scheduleDTO.getEmployeeIds()
+                 ) {
+                employees.add(employeeService.findEmployeeById(employeeid));
+            }
+            schedule.setEmployees(employees);
+        }
+        if(scheduleDTO.getPetIds() != null){
+            List<Pet> pets = new ArrayList<>();
+            for (Long petId: scheduleDTO.getPetIds()
+                 ) {
+                pets.add(petService.findPetById(petId));
+            }
+            schedule.setPets(pets);
+        }
+        return schedule;
+    }
+    private ScheduleDTO convertToScheduleDto(Schedule schedule){
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        BeanUtils.copyProperties(schedule, scheduleDTO);
+        if(schedule.getEmployees() != null){
+            List<Long> employeeids = new ArrayList<>();
+            for (Employee employee: schedule.getEmployees()
+                 ) {
+                employeeids.add(employee.getId());
+            }
+            scheduleDTO.setEmployeeIds(employeeids);
+        }
+
+        if(schedule.getPets() != null){
+            List<Long> petids = new ArrayList<>();
+            for (Pet pet: schedule.getPets()
+                 ) {
+                petids.add(pet.getId());
+            }
+            scheduleDTO.setPetIds(petids);
+        }
+        return scheduleDTO;
     }
 }
