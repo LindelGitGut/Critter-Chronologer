5b4a9589ddd6f2c8e5c23f7699a54c0cf85dda3e
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-04-12 05:25:27 +0200

    created repo for Schedule

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -0,0 +1,6 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository {
+}
